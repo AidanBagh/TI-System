7554eeb268106de4b52f63b883dc1b907e2cd271
AidanBagh <133143259+AidanBagh@users.noreply.github.com>
2024-01-26 10:01:58 +0200

    Add files via upload

---
+++ src/components/Dashboard/DashboardClient.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './client.css'
+
+const DashboardClient = () => {
+  return (
+    <div> 0781003078
+      <h1>Dashboard Client</h1>
+      {/* Your home page content goes here */}
+      <button className='redo-btn'>Redo</button>
+    </div>
+  );
+};
+
+export default DashboardClient;
---
+++ src/components/Dashboard/DashboardDriver.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+
+const DashboardDriver = () => {
+  return (
+    <div>
+      <h1>DashBoard Driver</h1>
+      {/* Your home page content goes here */}
+    </div>
+  );
+};
+
+export default DashboardDriver;
