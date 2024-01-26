0a1cd96196ef17661b0f0e6c429ae4a72101bdef
AidanBagh <133143259+AidanBagh@users.noreply.github.com>
2024-01-26 19:14:04 +0200

    Update Reusable.js

--- src/components/Dashboard/Client/Reusable.js
+++ src/components/Dashboard/Client/Reusable.js
@@ -1,72 +1,46 @@
-// Reusable2.js
+//Reusable.js
 
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useState } from 'react';
 import './client.css'
-import { FaUser } from 'react-icons/fa';
+import { GoogleBox,  InfoBoxKigali} from './Reusable2';
+import { Link } from 'react-router-dom';
 
 
-const OpenStreetMap = ({ latitude, longitude, zoom }) => {
+export const KigaliPage = () => {
   return (
-    <MapContainer
-      center={[latitude, longitude]}
-      zoom={zoom}
-      style={{ width: '100%', height: '100%'}}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+    <div className='kigaliBox'>
+      <div className='infoBox'>
+      <InfoBoxKigali />
+      </div>
+      <div className='googleBox'>
+        <GoogleBox />
+      </div>
+    </div>
   );
 };
 
 
-export const GoogleBox = () => {
-    // Replace with your actual latitude, longitude, and zoom values
-    const latitude = -1.9706;
-    const longitude = 30.1044;
-    const zoom = 12;
-  
+ export const OutsideKigaliPage = () => {
     return (
-      <div className='googleBox'>
-        <OpenStreetMap latitude={latitude} longitude={longitude} zoom={zoom} />
-      </div>
-    );
-  };
-
-  const ProfileBox = () => {
-    // Placeholder data
-    const profilePictureUrl = 'https://example.com/profile-picture.jpg'; // Replace with actual URL
-    const name = 'John Doe';
-    const price = '$50';
-    const address = '123 Main St, City';
-  
-    return (
-      <div className="profile-box">
-        <div className="profile-picture">
-          <FaUser/>
-        </div>
-        <div className="profile-info">
-          <div className="name">{name}</div>
-          <div className="address">{address}</div>
-        </div>
-        <div className="price">{price}</div>
-      </div>
-    );
-  };
-
-
-  export const InfoBoxKigali = () =>{
-    return(
+      <div className='kigaliBox'>
       <div className='infoBox'>
-        <h3>Kigali Services Screen</h3>
-        <div className='profilesBox'>
-          <ProfileBox/>
-          <ProfileBox/>
-          <ProfileBox/>
-        </div>
+        
       </div>
-    )
-  }
+      <div className='googleBox'>
+        <GoogleBox />
+      </div>
+    </div>
+    );
+  };
+
+
+  export const RentMyCarPage = () => {
+    return (
+      <div className='kigaliBox'>
+      <div className='infoBox'>
+        
+      </div>
+    </div>
+    );
+  };
+  
