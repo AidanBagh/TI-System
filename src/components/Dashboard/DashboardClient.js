635d9c718ebf56d7a61340711f69b29384645c84
AidanBagh <133143259+AidanBagh@users.noreply.github.com>
2024-01-26 18:29:57 +0200

    Update DashboardClient.js

--- src/components/Dashboard/DashboardClient.js
+++ src/components/Dashboard/DashboardClient.js
@@ -1,8 +1,9 @@
-// Dashboard.js
+// DashboardClient.js
 import './DashClient.css'
 import React, { useState } from 'react';
 import { KigaliPage, OutsideKigaliPage, RentMyCarPage } from './Client/Reusable';
 import { Link } from 'react-router-dom';
+import { FaSignOutAlt, FaUserAlt , FaCarAlt, FaBusAlt, FaBuilding} from 'react-icons/fa';
 
 const DashboardClient = () => {
   const [selectedOption, setSelectedOption] = useState('Kigali');
@@ -15,10 +16,11 @@ const DashboardClient = () => {
     <div>
       <div style={{ display: 'flex', height: '100vh' }} className='dashclient'>
         <div className='ver-navbar'>
-          <p className={selectedOption === 'Kigali' ? 'selected' : ''} onClick={() => handleOptionClick('Kigali')}>Kigali</p>
-          <p className={selectedOption === 'OutsideKigali' ? 'selected' : ''} onClick={() => handleOptionClick('OutsideKigali')}>Outside Kigali</p>
-          <p className={selectedOption === 'RentMyCar' ? 'selected' : ''} onClick={() => handleOptionClick('RentMyCar')}>Rent My Car</p>
-          <p><Link to="/login">Log Out</Link></p>
+          <FaUserAlt size={24} color='#6c72ff' className='userIcon'/>
+          <p className={selectedOption === 'Kigali' ? 'selected' : ''} onClick={() => handleOptionClick('Kigali')}><FaBuilding/></p>
+          <p className={selectedOption === 'OutsideKigali' ? 'selected' : ''} onClick={() => handleOptionClick('OutsideKigali')}><FaBusAlt/></p>
+          <p className={selectedOption === 'RentMyCar' ? 'selected' : ''} onClick={() => handleOptionClick('RentMyCar')}><FaCarAlt/></p>
+          <p><Link to="/login"><FaSignOutAlt/></Link></p>
         </div>
         <div style={{ flex: 1, padding: '10px' }}>
           {selectedOption === 'Kigali' && <KigaliPage />}
