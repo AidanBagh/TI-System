376f7c723e0b00846cb01b4adbf634f99894d7ae
AidanBagh <133143259+AidanBagh@users.noreply.github.com>
2024-01-26 17:37:45 +0200

    Update DashboardClient.js

--- src/components/Dashboard/DashboardClient.js
+++ src/components/Dashboard/DashboardClient.js
@@ -2,6 +2,7 @@
 import './DashClient.css'
 import React, { useState } from 'react';
 import { KigaliPage, OutsideKigaliPage, RentMyCarPage } from './Client/Reusable';
+import { Link } from 'react-router-dom';
 
 const DashboardClient = () => {
   const [selectedOption, setSelectedOption] = useState('Kigali');
@@ -17,6 +18,7 @@ const DashboardClient = () => {
           <p className={selectedOption === 'Kigali' ? 'selected' : ''} onClick={() => handleOptionClick('Kigali')}>Kigali</p>
           <p className={selectedOption === 'OutsideKigali' ? 'selected' : ''} onClick={() => handleOptionClick('OutsideKigali')}>Outside Kigali</p>
           <p className={selectedOption === 'RentMyCar' ? 'selected' : ''} onClick={() => handleOptionClick('RentMyCar')}>Rent My Car</p>
+          <p><Link to="/login">Log Out</Link></p>
         </div>
         <div style={{ flex: 1, padding: '10px' }}>
           {selectedOption === 'Kigali' && <KigaliPage />}
